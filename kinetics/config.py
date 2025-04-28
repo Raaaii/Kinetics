55da8542ecd42293b3d7b009efa3f5b4abb265a5
Rajka <p.r.rajka@gmail.com>
2025-04-28 10:16:11 +0200

    docs

--- kinetics/config.py
+++ kinetics/config.py
@@ -8,16 +8,16 @@ DEFAULT_SHEET_NAME = "Sheet1"
 INIT_PARAMS_2SPECIES = {
     "K1": {"value": 1e-6, "min": 1e-7, "max": 1e-5},
     "K2": {"value": 1e-6, "min": 1e-7, "max": 1e-5},
-    "offset": {"value":  2.1796e-04},
+    "offset": {"value": 2.1796e-04},
     "scale": {"value": 0.1},
 }
 
-# === Initial Parameters for 3-Species Model ===
+# === Initial Parameters for 3-Species Model  ==
 INIT_PARAMS_3SPECIES = {
     "K1": {"value": 1e-6, "min": 1e-10, "max": 1e2},
     "K2": {"value": 1e-6, "min": 1e-10, "max": 1e2},
     "K3": {"value": 1e-6, "min": 1e-10, "max": 1e2},
-    "offset": {"value": 2.1796e-04 },
+    "offset": {"value": 2.1796e-04},
     "scale": {"value": 0.1},
 }
 
